1f1bd3a42465bffb709c97d5b977b3e7e8400c7f
Arthur Cerveira <aacerveira@inf.ufpel.edu.br>
2023-12-14 17:15:59 -0300

    Assess baselines and compile results to file

---
+++ assess_baselines.py
@@ -0,0 +1,136 @@
+import os
+
+from guacamol.assess_goal_directed_generation import assess_goal_directed_generation
+from baselines.graph_ga.goal_directed_generation import GB_GA_Generator
+from baselines.smiles_ga.goal_directed_generation import ChemGEGenerator
+from baselines.smiles_lstm_ppo.goal_directed_generation import PPODirectedGenerator
+from baselines.graph_mcts.goal_directed_generation import GB_MCTS_Generator
+from baselines.random_smiles_sampler.goal_directed_generation import RandomSmilesSampler
+from baselines.random_smiles_sampler.optimizer import RandomSamplingOptimizer
+from baselines.smiles_lstm_hc.goal_directed_generation import SmilesRnnDirectedGenerator
+from baselines.best_from_chembl.optimizer import BestFromChemblOptimizer
+from baselines.best_from_chembl.chembl_file_reader import ChemblFileReader
+
+SMILES_HTS_FILE = 'guacamol/data/guacamol_v1_50k.smiles'
+
+print("Generating molecules with GB_GA_Generator")
+
+optimiser = GB_GA_Generator(
+    smi_file=SMILES_HTS_FILE,
+    population_size=100,
+    offspring_size=200,
+    generations=1000,
+    mutation_rate=0.01,
+    patience=5,
+    n_jobs=-1,
+    random_start=False,
+)
+
+json_file_path = os.path.join("reports", 'graph_ga.json')
+
+assess_goal_directed_generation(
+    optimiser, json_output_file=json_file_path, benchmark_version="multitarget"
+)
+
+print("Generating molecules with ChemGEGenerator")
+
+optimiser = ChemGEGenerator(
+    smi_file=SMILES_HTS_FILE,
+    population_size=100,
+    n_mutations=200,
+    gene_size=300,
+    generations=1000,
+    n_jobs=-1,
+    random_start=False,
+    patience=5
+)
+
+json_file_path = os.path.join("reports", 'smiles_ga.json')
+
+assess_goal_directed_generation(
+    optimiser, json_output_file=json_file_path, benchmark_version="multitarget"
+)
+
+print("Generating molecules with PPODirectedGenerator")
+
+model_path = os.path.join("baselines/smiles_lstm_ppo/", 'pretrained_model', 'model_final_0.473.pt')
+
+optimiser = PPODirectedGenerator(
+    pretrained_model_path=model_path,
+    num_epochs=20,
+    episode_size=8192,
+    batch_size=1024,
+    entropy_weight=1,
+    kl_div_weight=10,
+    clip_param=0.2
+)
+
+json_file_path = os.path.join("reports", 'smiles_lstm_ppo.json')
+
+assess_goal_directed_generation(
+    optimiser, json_output_file=json_file_path, benchmark_version="multitarget"
+)
+
+# TODO
+# optimiser = GB_MCTS_Generator(
+#     pickle_directory="baselines/graph_mcts/",
+#     n_jobs=16,
+#     num_sims=40,
+#     max_children=25,
+#     init_smiles='CC',
+#     max_atoms=60,
+#     generations=1000,
+#     population_size=100,
+# )
+
+# json_file_path = os.path.join("reports", 'graph_mcts.json')
+
+# assess_goal_directed_generation(
+#     optimiser, json_output_file=json_file_path, benchmark_version="multitarget"
+# )
+
+print("Generating molecules with RandomSmilesSampler")
+
+with open(SMILES_HTS_FILE, 'r') as smiles_file:
+    smiles_list = smiles_file.readlines()
+
+sampler = RandomSmilesSampler(molecules=smiles_list)
+
+optimiser = RandomSamplingOptimizer(sampler=sampler)
+
+json_file_path = os.path.join("reports", 'random_smiles.json')
+
+assess_goal_directed_generation(
+    optimiser, json_output_file=json_file_path, benchmark_version="multitarget"
+)
+
+print("Generating molecules with BestFromChemblOptimizer")
+
+smiles_reader = ChemblFileReader(SMILES_HTS_FILE)
+
+optimizer = BestFromChemblOptimizer(smiles_reader=smiles_reader, n_jobs=-1)
+
+json_file_path = os.path.join("reports", 'best_from_chembl.json')
+
+assess_goal_directed_generation(optimizer, json_output_file=json_file_path, benchmark_version="multitarget")
+
+print("Generating molecules with SmilesRnnDirectedGenerator")
+
+model_path = os.path.join("baselines/smiles_lstm_hc", 'pretrained_model', 'model_final_0.473.pt')
+
+optimizer = SmilesRnnDirectedGenerator(pretrained_model_path=model_path,
+                                        n_epochs=20,
+                                        mols_to_sample=1024,
+                                        keep_top=512,
+                                        optimize_n_epochs=2,
+                                        max_len=100,
+                                        optimize_batch_size=256,
+                                        number_final_samples=4096,
+                                        random_start=False,
+                                        smi_file=SMILES_HTS_FILE,
+                                        n_jobs=-1)
+
+json_file_path = os.path.join("reports", 'smiles_lstm_hc.json')
+assess_goal_directed_generation(optimizer, json_output_file=json_file_path, benchmark_version="multitarget")
+
+print("Done!")
---
+++ compile_results.py
@@ -0,0 +1,113 @@
+import json
+import numpy as np
+import pandas as pd
+
+
+METHODS = {
+    "LSTM-PPO": "reports/smiles_lstm_ppo.json",
+    "LSTM-HC": "reports/smiles_lstm_hc.json",
+    "SMILES GA": "reports/smiles_ga.json",
+    "Graph GA": "reports/graph_ga.json",
+    "Best of dataset": "reports/best_from_chembl.json",
+}
+
+metric_order = [
+    "Score",
+    "Target Response",
+    "Blood-Brain Barrier",
+    "QED Score",
+    "CNS MPO",
+    "Synthetic Accessibility",
+]
+
+benchmark_order = [
+    "Alzheimer MPO",
+    "Schizophrenia MPO",
+    "Lung cancer MPO"
+]
+
+report_results = list()
+
+
+def get_metadata_keys(metadata):
+    keys = list(metadata.keys())
+
+    target = [k for k in keys if "GeometricMeanScoringFunction" in k]
+    target_scores = metadata[target[0]] if len(target) > 0 else None
+        
+    qed = [k for k in keys if "RdkitScoringFunction" in k]
+    qed_scores = metadata[qed[0]] if len(qed) > 0 else None
+
+    bbb = [k for k in keys if "TargetResponseScoringFunction" in k]
+    bbb_scores = metadata[bbb[0]] if len(bbb) > 0 else None
+
+    sa = [k for k in keys if "SyntheticAccessibilityScoringFunction" in k]
+    sa_scores = metadata[sa[0]] if len(sa) > 0 else None
+
+    cns = [k for k in keys if "CNS_MPO_ScoringFunction" in k]
+    cns_scores = metadata[cns[0]] if len(cns) > 0 else None
+
+    return {
+        "Target Response": target_scores,
+        "Blood-Brain Barrier": bbb_scores,
+        "QED Score": qed_scores,
+        "Synthetic Accessibility": sa_scores,
+        "CNS MPO": cns_scores        
+    }
+
+
+for method in METHODS:
+    report = METHODS[method]
+
+    with open(report) as f:
+        results = json.load(f)["results"]
+        
+        for result in results:
+            results_info = dict()
+
+            results_info["Method"] = method
+            results_info["Benchmark"] = result["benchmark_name"]
+            results_info["Score"] = f"{result['score']:.5f}"
+
+            metadata = result["metadata"]
+            scores_dict = get_metadata_keys(metadata)
+
+            for score_key in scores_dict:
+                scores = scores_dict[score_key]
+                if scores is None:
+                    continue
+
+                mean_score = np.mean(scores)
+                std_score = np.std(scores)
+
+                results_info[score_key] = f"{mean_score:.3f} ± {std_score:.3f}"
+            
+            report_results.append(results_info.copy())
+
+df = pd.DataFrame(report_results)   
+
+# Reorder the benchmarks
+df["Benchmark"] = pd.Categorical(df["Benchmark"], benchmark_order, ordered=True)
+
+dfs = list()
+
+for metric in metric_order:
+    df_score = df.pivot_table(
+        index=["Benchmark"],
+        columns=["Method"],
+        values=[metric],
+        aggfunc=lambda x: x,
+    )
+
+    df_score.columns = df_score.columns.droplevel(0)
+    df_score["Metric"] = metric
+
+    dfs.append(df_score)
+
+concat_dfs = pd.concat(dfs, axis=0).sort_values(by=["Benchmark"])
+
+concat_dfs["Metric"] = pd.Categorical(
+    concat_dfs["Metric"], categories=metric_order, ordered=True
+)
+
+concat_dfs[['Metric'] + list(METHODS.keys())].to_csv('reports/MTDD-Benchmarks.csv')
